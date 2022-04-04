eb6ac80af03820bc70a5d5d9005ee1002d563b5d
Victoria Olumide <olumideronke@gmail.com>
2022-04-04 08:17:23 -0700

    let try

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Main from './components/main';
+import topIntern from './images/TopIntern.svg';
+import './styles/app.scss';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faClose, faBars} from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Top'>
+        <img src={topIntern} alt="topIntern_logo" className='topLogo'/>
+        <div className='menuAll'>
+        <FontAwesomeIcon icon={faBars} className='bars open'/>
+          
+          <div className='menu'>
+          <FontAwesomeIcon icon={faClose} className='bars close'/>
+            <ul className='menuclick'>
+                <li><a>Home</a></li>
+                <li><a>Portfolio</a></li>
+                <li><a>Blog</a></li>
+                <li><a>عربي</a></li>
+            </ul>
+        </div>
+        </div>
+      </div>
+      
+      <Main/>
     </div>
   );
 }
